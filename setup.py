09bb52a760ae0b83084785a36053756c1fefd709
Haukur Kristinsson <haukur@hauxi.is>
2015-01-29 14:44:27 +0000

    reqs now empty.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="@haukurk",
     author_email="haukur@hauxi.is",
     packages=["nmapdb"],
-    install_requires=["requests"],
+    install_requires=[],
     url='https://github.com/haukurk/nmapdb',
     download_url='https://github.com/haukurk/nmapdb/releases/tag/v0.2.1',
     keywords=['nmap', 'sqlite', 'database'],
